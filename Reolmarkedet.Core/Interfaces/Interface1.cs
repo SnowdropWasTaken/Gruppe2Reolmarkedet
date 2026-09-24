e71ab778a159a9688bd7c726cb7d56e4a9c01763
kmchng <kmchng@outlook.com>
2026-09-24 12:33:15 +0200

    Add IGenericRepo<T> interface and update csproj
    Introduced IGenericRepo<T> in Reolmarkedet.Core.Interfaces for generic CRUD operations. Removed explicit Interfaces\ folder entry from Reolmarkedet.Core.csproj.

---
+++ Reolmarkedet.Core/Interfaces/Interface1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reolmarkedet.Core.Interfaces
+{
+    public interface IGenericRepo<T>
+    {
+        T Get(int id);
+        
+        IEnumerable<T> GetAll();
+        
+        void Add(T item);
+
+        void Remove(T item);
+
+        void Remove(int id);
+    }
+}
